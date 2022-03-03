06ca3b4e335d17d06d40760e3f0c8f77a7140af9
Jose Antonio Garcia <joangaes@Joses-MacBook-Pro.local>
2022-03-03 01:13:01 +0100

    deploy

--- app.py
+++ app.py
@@ -132,6 +132,10 @@ external_stylesheets = [
 ]
 
 app= dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+from app import index
+
 app.title= "Digital currency webapp"
 app.layout=html.Div([html.H1(children="Digital currency web-app", className="header-title"),
                     html.P(children="Analyze the survey result", className= "header-description",
